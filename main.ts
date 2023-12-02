ab865d69a3de0f24b7bfd827226e8034c304fa1d
Dave Shepard <kynatro@gmail.com>
2023-12-01 23:56:34 -0800

    Replace sample code with Obsidian Notepack template starter

--- main.ts
+++ main.ts
@@ -1,89 +1,95 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile, TFolder, TooltipOptions, normalizePath } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+type ObsidianSettingType = 'text' | 'toggle';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ObsidianNotepackSettings {
+	enableTemplate: boolean;
+	templateFilename: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface ObsidianNotepackSetting {
+	default: string | boolean;
+	desc: string;
+	keyName: keyof ObsidianNotepackSettings;
+	name: string;
+	placeholder?: string;
+	tooltip?: string;
+	tooltipOptions?: TooltipOptions;
+	type: ObsidianSettingType;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const PLUGIN_SETTINGS: Array<ObsidianNotepackSetting> = [
+	{
+		desc: 'Enable automatic templates to be applied to new files. Finds the closest file matching the "Template filename" option and pre-populates the new file with the template file\'s contents.',
+		name: 'Enable templates',
+		keyName: 'enableTemplate',
+		type: 'toggle',
+		default: true
+	},
+	{
+		desc: 'File name to search for in folders and use as a template',
+		name: 'Template filename',
+		keyName: 'templateFilename',
+		placeholder: 'Enter a filename',
+		type: 'text',
+		default: '.template'
+	}
+]
+
+const DEFAULT_SETTINGS = PLUGIN_SETTINGS.reduce((obj, setting: ObsidianNotepackSetting) => ({
+	...obj,
+	[setting.keyName]: setting.default
+}), {})
+
+export default class ObsidianNotepack extends Plugin {
+	settings: ObsidianNotepackSettings;
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.app.workspace.onLayoutReady(() => {
+			this.registerEvent(this.app.vault.on('create', this.handleCreateFile.bind(this)))
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addSettingTab(new ObsidianNotepackSettingTab(this.app, this));
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+	async getNearestTemplate(folder: TFolder): Promise<string | undefined> {
+		const templatePath = await this.getNearestTemplatePath(folder);
+
+		if (templatePath) {
+			return this.app.vault.adapter.read(templatePath);
+		}
+	}
+
+	async getNearestTemplatePath(folder: TFolder): Promise<string | undefined> {
+		const templatePath = normalizePath([folder.path, this.settings.templateFilename].join('/'));
+
+		if (await this.app.vault.adapter.exists(templatePath)) {
+			return templatePath;
+		} else {
+			if (folder.parent) {
+				return this.getNearestTemplatePath(folder.parent);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	async handleCreateFile(file: TFile) {
+		const { parent } = file;
+
+		if (this.settings.enableTemplate) {
+			if (parent) {
+				const template = await this.getNearestTemplate(parent);
+
+				if (template) {
+					await file.vault.adapter.write(normalizePath(file.path), template);
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		}
 	}
 
 	async saveSettings() {
@@ -91,44 +97,63 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ObsidianNotepackSettingTab extends PluginSettingTab {
+	plugin: ObsidianNotepack;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianNotepack) {
 		super(app, plugin);
+
 		this.plugin = plugin;
 	}
 
+	settingFactory(container: HTMLElement, config: ObsidianNotepackSetting) {
+		const { desc, keyName, name, type } = config;
+		const setting = new Setting(container)
+			.setName(name)
+			.setDesc(desc)
+
+		switch (type) {
+			case 'text':
+				setting.addText(text => {
+					if (config.placeholder) {
+						text.setPlaceholder(config.placeholder)
+					}
+
+					text.setValue(this.plugin.settings[keyName].toString())
+					text.onChange(async (value) => {
+						this.plugin.settings = {
+							...this.plugin.settings,
+							[keyName]: value
+						};
+						await this.plugin.saveSettings();
+					});
+				})
+			break;
+
+			case 'toggle':
+				setting.addToggle(toggle => {
+					if (config.tooltip) {
+						toggle.setTooltip(config.tooltip, config.tooltipOptions);
+					}
+
+					toggle.setValue(Boolean(this.plugin.settings[keyName]))
+					toggle.onChange(async (value) => {
+						this.plugin.settings = {
+							...this.plugin.settings,
+							[keyName]: value
+						};
+						await this.plugin.saveSettings();
+					})
+				})
+			break;
+		}
+	}
+
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		PLUGIN_SETTINGS.forEach(setting => this.settingFactory(containerEl, setting));
 	}
 }
